c3f669b0cd9dcac484db5b570e673333c54d5015
Patricio Cortes <patricio.cortes@noirlab.edu>
2022-07-08 11:23:00 -0400

    Remove NiFpga_MergeStatus

--- src/crioMain.c
+++ src/crioMain.c
@@ -16,21 +16,19 @@ int main(){
     NiFpga_Status status = NiFpga_Initialize();
     if (NiFpga_IsNotError(status)) {
         NiFpga_Session session;
-        // string bitfile = NiFpga_mainFPGA_Bitfile;
         // Open a session and download the bitstream
-        NiFpga_MergeStatus(&status,
-                           NiFpga_Open(NiFpga_mainFPGA_Bitfile,
+        NiFpga_Open(NiFpga_mainFPGA_Bitfile,
                            NiFpga_mainFPGA_Signature,
                            "RIO0",
-                           NiFpga_OpenAttribute_NoRun, &session));
+                           NiFpga_OpenAttribute_NoRun, &session);
  
         if (NiFpga_IsNotError(status)) {
             // Run the FPGA VI on the target
-            NiFpga_MergeStatus(&status, NiFpga_Run(session, 0));
+            NiFpga_Run(session, 0);
 
             /* Reset UserLED0 */
-            NiFpga_MergeStatus(&status, NiFpga_WriteBool(session, NiFpga_mainFPGA_ControlBool_UserLED0,0));
-            sleep(2);
+            NiFpga_WriteBool(session, NiFpga_mainFPGA_ControlBool_UserLED0,0);
+            sleep(1);
 
             // UserSwitches Initialization
             NiFpga_Bool userSw0 = 0;
@@ -44,33 +42,63 @@ int main(){
             int16_t input[SIZE];  /// input from FPGA FIFO_B
 
             /* LEDO = on */
-            NiFpga_MergeStatus(&status, NiFpga_WriteBool(session, NiFpga_mainFPGA_ControlBool_UserLED0,1));
+            NiFpga_WriteBool(session, NiFpga_mainFPGA_ControlBool_UserLED0,1);
 
             int x = 0;
-            while (x < 10)
+            while (x < 10000)
             {
+                
                 // Read UserSwitches
-                NiFpga_MergeStatus(&status, NiFpga_ReadBool(session, NiFpga_mainFPGA_IndicatorBool_UserSwitch0,&userSw0));
-                NiFpga_MergeStatus(&status, NiFpga_ReadBool(session, NiFpga_mainFPGA_IndicatorBool_UserSwitch1,&userSw1));
-                NiFpga_MergeStatus(&status, NiFpga_ReadBool(session, NiFpga_mainFPGA_IndicatorBool_UserSwitch2,&userSw2));
-                NiFpga_MergeStatus(&status, NiFpga_ReadBool(session, NiFpga_mainFPGA_IndicatorBool_UserSwitch3,&userSw3));   
+                NiFpga_ReadBool(session, NiFpga_mainFPGA_IndicatorBool_UserSwitch0,&userSw0);
+                NiFpga_ReadBool(session, NiFpga_mainFPGA_IndicatorBool_UserSwitch1,&userSw1);
+                NiFpga_ReadBool(session, NiFpga_mainFPGA_IndicatorBool_UserSwitch2,&userSw2);
+                NiFpga_ReadBool(session, NiFpga_mainFPGA_IndicatorBool_UserSwitch3,&userSw3);   
             
-                //cout << "userSw0=" << ((userSw0) ? "t" : "f") << " " << hex << (int)userSw0;
                 printf("iteration = %d\t userSw0 = %d\t userSw1 = %d\t userSw2 = %d\t userSw3 = %d\n", x, (int)userSw0, (int)userSw1, (int)userSw2, (int)userSw3);
 
+                // copy FIFO data to FPGA FIFO_A
+                NiFpga_WriteFifoI16(session, 
+                                            NiFpga_mainFPGA_HostToTargetFifoI16_FIFO_A,
+                                            output, 
+                                            SIZE, 
+                                            NiFpga_InfiniteTimeout, 
+                                            NULL);
+
+                // copy FIFO data from the FPGA FIFO_B
+                NiFpga_ReadFifoI16(session, 
+                                            NiFpga_mainFPGA_TargetToHostFifoI16_FIFO_B, 
+                                            input,
+                                            SIZE, 
+                                            NiFpga_InfiniteTimeout, 
+                                            NULL);
+
+                /*char str = " input=";
+                for (int j = 0; j < SIZE; ++j) {
+                    str += to_string(input[j]) + " ";
+                }
+                
+                printf("str =%c\t", str);
+
+                str = "output=";
+                for (int j = 0; j < SIZE; ++j) {
+                    str += to_string(output[j]) + " ";
+                }
+                cout << str << endl;
+                */
+                
                 x++;
-                sleep(1);
-                                                   
+                sleep(0.1);
+                
             }
             /* LEDO = off */
-            NiFpga_MergeStatus(&status, NiFpga_WriteBool(session, NiFpga_mainFPGA_ControlBool_UserLED0,0));
+            NiFpga_WriteBool(session, NiFpga_mainFPGA_ControlBool_UserLED0,0);
 
             // Close the session to the FPGA VI
-            NiFpga_MergeStatus(&status, NiFpga_Close(session, 0));
+            NiFpga_Close(session, 0);
         }
         
         // Close the FPGA interface.
-        NiFpga_MergeStatus(&status, NiFpga_Finalize());
+        NiFpga_Finalize();
     }    
      
     // check if anything went wrong
