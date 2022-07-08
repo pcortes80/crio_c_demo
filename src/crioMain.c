c7bcdb6c485be0ea770ddcf759f3fdcea2b17fd5
Patricio Cortes <patricio.cortes@noirlab.edu>
2022-07-08 17:14:56 -0400

    last test ok

--- src/crioMain.c
+++ src/crioMain.c
@@ -1,14 +1,15 @@
 /* DEMO to understand how to use the C API Generator
    to communicate RT Processor and FPGA.
    Target: cRIO-9033 Module NI-9344 on Slot1
+   Patricio Cortes - July 2022
 */
 
-// System headers
+/* System headers */
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
 
-// Project headers
+/* Project headers */
 #include "../include/NiFpga_mainFPGA.h"
 
 int main(){
@@ -17,32 +18,31 @@ int main(){
     printf("Turn UserSwitch0 ON and Press <Enter> to begin...\n");
     getchar(); 
 
-    // Initialize the FPGA interface 
+    /* Initialize the FPGA interface */
     NiFpga_Status status = NiFpga_Initialize();
     if (NiFpga_IsNotError(status)) {
         NiFpga_Session session;
-        // Open a session and download the bitstream
+        /* Open a session and download the bitstream */
         NiFpga_Open(NiFpga_mainFPGA_Bitfile,
                            NiFpga_mainFPGA_Signature,
                            "RIO0",
                            NiFpga_OpenAttribute_NoRun, &session);
  
         if (NiFpga_IsNotError(status)) {
-            // Run the FPGA VI on the target
+            /* Run the FPGA VI on the target */
             NiFpga_Run(session, 0);
 
             /* Reset UserLED0 */
             NiFpga_WriteBool(session, NiFpga_mainFPGA_ControlBool_UserLED0,0);
             sleep(1);
 
-            // UserSwitches Initialization
+            /* UserSwitches Initialization */
             NiFpga_Bool userSw0 = 0;
             NiFpga_Bool userSw1 = 0;
             NiFpga_Bool userSw2 = 0;
             NiFpga_Bool userSw3 = 0;
 
             const int SIZE = 256; /// size of FIFOs
-            uint8_t inputSwitches[SIZE]; /// input form FPGA switches
             int16_t output[SIZE]; /// output to FPGA FIFO_A
             int16_t input[SIZE];  /// input from FPGA FIFO_B
 
@@ -67,18 +67,15 @@ int main(){
                 */
 
                 int i,j;
-                    
-                /* initialize elements of FIFO_A buffer */         
-                /*
+
+                /* put identifiable values in the output buffer */
                 printf("Output=\t");
-                for (i = 0; i < SIZE; i++) {
-                    output[i] = i + 10; // set element at location i to i + 1
-                    printf("%d\t", output[i]);
-                    }
-                               
+                for (j = 1; j < SIZE; ++j) { 
+                    output[j] = output[j-1] + 1;
+                    printf("%d\t", output[j]);
+                }
                 printf("\n");
-                */
-                
+
                 // copy FIFO data to FPGA FIFO_A
                 NiFpga_WriteFifoI16(session, 
                                             NiFpga_mainFPGA_HostToTargetFifoI16_FIFO_A,
@@ -95,35 +92,24 @@ int main(){
                                             NiFpga_InfiniteTimeout, 
                                             NULL);
 
-
-                /* print each FIFO element's value */
+                /* print each FIFO element's value from FIFO_B */
                 printf("Input=\t");
                 for (j = 0; j < SIZE; ++j) {
                     printf("%d\t", input[j]);
                 }                            
                 printf("\n");
 
-
-                printf("Output=\t");
-                for (j = 1; j < SIZE; ++j) { 
-                    output[j] = output[j-1] + 1;
-                    printf("%d\t", output[j]);
-                }
-                printf("\n");
-
                 /* Change the output for the next loop */
-                /*
                 output[0] = output[SIZE - 1];
                 if (output[0] > 20000) output[0] = -20000;
                 for (j = 1; j < SIZE; ++j) {
                     output[j] = output[j-1] + 1;
                 }
-                */
-
+                
                 /* 100 ms delay */
                 sleep(0.100);
 
-                /* Check if UserSwitch0 is ON */
+                /* Check if UserSwitch0 is ON to continue, OFF to stop*/
                 NiFpga_ReadBool(session, NiFpga_mainFPGA_IndicatorBool_UserSwitch0,&userSw0);
 
             } while ((int)userSw0 == 1);
